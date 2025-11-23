246c251e5cfdc2282d54932170d1d63370ea451b
oboyle3 <oboyle3patrick@gmail.com>
2025-11-22 19:23:21 -0500

    added dashboard page

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     return render(request, "core/homepage.html")
+
+def dashboard(request):
+    return render(request,"core/dashboard.html")
--- stockwatch/urls.py
+++ stockwatch/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core.views import homepage#added
+from core.views import dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',homepage, name='homepage'),
+    path('dashboard/',dashboard, name='dashboard'),
 ]
